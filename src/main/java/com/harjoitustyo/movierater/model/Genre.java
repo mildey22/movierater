4b0c636b442b7f6fce8714b31cb07a82c82b6ff3
Aatos Otronen <127504684+mildey22@users.noreply.github.com>
2024-04-20 17:53:54 +0300

    Created Genre.java

--- src/main/java/com/harjoitustyo/movierater/model/Genre.java
+++ src/main/java/com/harjoitustyo/movierater/model/Genre.java
@@ -1,5 +1,40 @@
 package com.harjoitustyo.movierater.model;
 
-public class Genre {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
+public class Genre {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long genreId;
+    private String genreName;
+
+    public Genre() {
+
+    }
+
+    public Genre(String genreName) {
+        super();
+        this.genreName = genreName;
+    }
+
+    public long getGenreId() {
+        return genreId;
+    }
+
+    public void setGenreId(long genreId) {
+        this.genreId = genreId;
+    }
+
+    public String getGenreName() {
+        return genreName;
+    }
+
+    public void setGenreName(String genreName) {
+        this.genreName = genreName;
+    }
+    
 }
