f0a6136e1e7a00ecb0a102d7c99e877fdd027446
Aatos Otronen <127504684+mildey22@users.noreply.github.com>
2024-04-20 18:03:25 +0300

    Crated Rating.java

--- src/main/java/com/harjoitustyo/movierater/model/Rating.java
+++ src/main/java/com/harjoitustyo/movierater/model/Rating.java
@@ -1,5 +1,58 @@
 package com.harjoitustyo.movierater.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
 public class Rating {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long ratingId;
+    private String rating;
+    @JsonIgnore
+    @OneToMany(mappedBy = "rating")
+    private List<Movie> movies;
+
+    public Rating() {
+
+    }
+
+    public Rating(String rating) {
+        super();
+        this.rating = rating;
+    }
+
+    public long getRatingId() {
+        return ratingId;
+    }
+
+    public void setRatingId(long ratingId) {
+        this.ratingId = ratingId;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public List<Movie> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(List<Movie> movies) {
+        this.movies = movies;
+    }
+
 
 }
